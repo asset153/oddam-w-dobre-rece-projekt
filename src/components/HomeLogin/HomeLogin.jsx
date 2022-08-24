2c3c68a506b32852ba0534e238a883f5548cafa9
asset153 <matadamczyk401@gmail.com>
2022-08-24 16:50:20 +0200

    refacting HomeHeader;  add structure to HomeLogin;

--- src/components/HomeLogin/HomeLogin.jsx
+++ src/components/HomeLogin/HomeLogin.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useId } from "react";
+import { useNavigate } from "react-router-dom";
 
 function HomeLogin() {
-  return <div>HomeLogin</div>;
+  const id = useId();
+  const navigate = useNavigate();
+
+  return (
+    <section className="loginContainer">
+      <article className="loginContainer__article">
+        <h3>Zaloguj się</h3>
+
+        <div className="loginContainer__article__fancy-border"></div>
+
+        <form className="loginContainer__article__form">
+          <div>
+            <label htmlFor={`email${id}`}>Email</label>
+            <input type="text" id={`email${id}`} />
+          </div>
+
+          <div>
+            <label htmlFor={`password${id}`}>Password</label>
+            <input type="text" id={`password${id}`} />
+          </div>
+        </form>
+
+        <div className="loginContainer__article__btns">
+          <button onClick={() => navigate("/rejestracja")}>Załóż konto</button>
+          <button>Zaloguj się</button>
+        </div>
+      </article>
+    </section>
+  );
 }
 
 export default HomeLogin;
