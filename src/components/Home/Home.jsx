281ed89833befec8549ddab4d788cf89c8c94902
asset153 <matadamczyk401@gmail.com>
2022-08-26 17:16:00 +0200

    create section Three Columns; add styling; add assets;

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HomeHeader from "../HomeHeader/HomeHeader";
+import HomeSectionThreeColumns from "../HomeSectionThreeColumns/HomeSectionThreeColumns";
 
 function Home() {
   return (
     <main className="mainContainer">
       <HomeHeader />
+      <HomeSectionThreeColumns />
     </main>
   );
 }
---
+++ src/components/HomeSectionThreeColumns/HomeSectionThreeColumns.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+function HomeSectionThreeColumns() {
+  return (
+    <section className="sectionThreeColumnsContainer">
+      <article className="sectionThreeColumnsContainer__article">
+        <span className="sectionThreeColumnsContainer__article__titleCount">
+          10
+        </span>
+        <span className="sectionThreeColumnsContainer__article__content">
+          ODDANYCH WORKÓW
+        </span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim
+          a elit viverra elementuma. Aliquam erat volutpat.
+        </p>
+      </article>
+
+      <article className="sectionThreeColumnsContainer__article">
+        <span className="sectionThreeColumnsContainer__article__titleCount">
+          5
+        </span>
+        <span className="sectionThreeColumnsContainer__article__content">
+          WSPARTYCH ORGANIZACJI
+        </span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim
+          a elit viverra elementuma. Aliquam erat volutpat.
+        </p>
+      </article>
+
+      <article className="sectionThreeColumnsContainer__article">
+        <span className="sectionThreeColumnsContainer__article__titleCount">
+          7
+        </span>
+        <span className="sectionThreeColumnsContainer__article__content">
+          ZORGANIZOWANY ZBIÓREK
+        </span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim
+          a elit viverra elementuma. Aliquam erat volutpat.
+        </p>
+      </article>
+    </section>
+  );
+}
+
+export default HomeSectionThreeColumns;
