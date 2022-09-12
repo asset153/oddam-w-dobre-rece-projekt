539a72a30f0c59beb48914a89fcc724b8bc894db
asset153 <matadamczyk401@gmail.com>
2022-09-12 13:53:28 +0200

    refactoring files;

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://vpvxtxuesfxscygkccny.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwdnh0eHVlc2Z4c2N5Z2tjY255Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjIyOTQ1MjYsImV4cCI6MTk3Nzg3MDUyNn0.I18_RfuPSfj-nqel_iVT_bHVWEmxjc_yBjIUG2C2BY0";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
